f73c073e8aad5912b86ab54b952688d2838f9e8f
shoce <yogze@icloud.com>
2024-04-11 17:27:08 +0530

    multiple words/columns to select

--- wo.go
+++ wo.go
@@ -19,44 +19,58 @@ import (
 	"strings"
 )
 
+const (
+	NL = "\n"
+)
+
 func main() {
 	var err error
-	var n int
+	var ijk []int
 	var l string
 	var ww []string
 
 	if len(os.Args) < 2 {
-		fmt.Fprintf(os.Stderr, "usage: wo n\n	n = position of the word to select, negative to select from the end of the line\n")
+		fmt.Fprintf(
+			os.Stderr,
+			"usage: wo i [j] [k] [...]"+NL+
+				"	i, j, k, ... = position of the word to select, negative to select from the end of the line"+NL,
+		)
 		os.Exit(1)
 	}
 
-	n, err = strconv.Atoi(os.Args[1])
-	if err != nil || n == 0 {
-		fmt.Fprintf(os.Stderr, "invalid n = '%s'", os.Args[1])
-		os.Exit(1)
+	for _, nn := range os.Args[1:] {
+		n, err := strconv.Atoi(nn)
+		if err != nil || n == 0 {
+			fmt.Fprintf(os.Stderr, "invalid number '%s': should be digital and non-zero", nn)
+			os.Exit(1)
+		}
+		ijk = append(ijk, n)
 	}
 
-	sp := regexp.MustCompile(`[ \t]+`)
+	sp := regexp.MustCompile(`[^ \t]+([ \t]+|$)`)
 	sc := bufio.NewScanner(os.Stdin)
 
 	for sc.Scan() {
 		l = sc.Text()
 		l = strings.TrimSpace(l)
 
-		ww = sp.Split(l, -1)
+		ww = sp.FindAllString(l, -1)
 
-		if n > 0 && len(ww) < n {
-			continue
-		}
-		if n < 0 && len(ww) < -n {
-			continue
-		}
+		for _, n := range ijk {
+			if n > 0 && len(ww) < n {
+				continue
+			}
+			if n < 0 && len(ww) < -n {
+				continue
+			}
 
-		if n > 0 {
-			fmt.Println(ww[n-1])
-		} else {
-			fmt.Println(ww[len(ww)+n])
+			if n > 0 {
+				fmt.Print(ww[n-1])
+			} else if n < 0 {
+				fmt.Print(ww[len(ww)+n])
+			}
 		}
+		fmt.Println()
 	}
 	if err = sc.Err(); err != nil {
 		fmt.Fprintf(os.Stderr, "read stdin: %v", err)
